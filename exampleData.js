d88d2a961fbc85449af03ab3587853c4198bb5b1
Cristiano da Silva Monteiro <cristianodasilvamonteiro@gmail.com>
2022-08-16 21:07:46 -0300

    exampleData.js adicionado

---
+++ exampleData.js
@@ -0,0 +1,21 @@
+const exampleLabels = ['A', 'B', 'C', 'D', 'E', 'F'];
+
+const exampleData = [20, 50, 10, 5, 80, 65];
+
+const exampleBackgroundColor = [
+    'rgba(255, 99, 132, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(255, 206, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)',
+    'rgba(153, 102, 255, 0.2)',
+    'rgba(255, 159, 64, 0.2)'
+];
+
+const exampleBorderColor = [
+    'rgba(255, 99, 132)',
+    'rgba(54, 162, 235)',
+    'rgba(255, 206, 86)',
+    'rgba(75, 192, 192)',
+    'rgba(153, 102, 255)',
+    'rgba(255, 159, 64)'
+];
